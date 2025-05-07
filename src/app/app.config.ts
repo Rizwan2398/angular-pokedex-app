58330da31957609f5dd5b57ad5d7ea39ff43a470
Rizwan2398 <abeedrizwan02@gmail.com>
2025-05-07 14:00:52 +0200

    gerer les erreurs 404

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,13 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, Routes } from '@angular/router';
 import { PokemonListComponent } from './pokemon/pokemon-list/pokemon-list.component';
 import { PokemonProfileComponent } from './profile/pokemon-profile/pokemon-profile.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  { path:'pokemons/:id', component:PokemonProfileComponent},
-  { path:'pokemons', component:PokemonListComponent},
+  { path:'pokemons/:id', component:PokemonProfileComponent, title: 'Pokemon'},
+  { path:'pokemons', component:PokemonListComponent, title: 'Pokédex'},
   { path: '', redirectTo: '/pokemons', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent},
 
 ]
 
--- src/app/profile/pokemon-profile/pokemon-profile.component.ts
+++ src/app/profile/pokemon-profile/pokemon-profile.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject, signal } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { PokemonService } from '../../pokemon.service';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-pokemon-profile',
-  imports: [],
+  imports: [RouterLink, DatePipe],
   templateUrl: './pokemon-profile.component.html',
   styles: ``
 })
